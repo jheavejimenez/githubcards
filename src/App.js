e6c04751b1a0481fdbed076c08d9e2d4761aee9b
jheave <jimenezjheave123@gmail.com>
2022-08-23 02:18:05 +0800

    refractor: class to functional component

--- src/App.js
+++ src/App.js
@@ -1,30 +1,47 @@
-import React, {Component} from "react";
+import React, {Component, useState} from "react";
 import CardList from "./components/CardList";
 import Form from "./components/Form";
 
-class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            profileData: []
-        }
-    }
-    addProfileCardData = (profile) => {
-        this.setState(prevState => {
-            return {
-                profileData: [...prevState.profileData, profile]
-            }
+// class App extends Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             profileData: []
+//         }
+//     }
+//     addProfileCardData = (profile) => {
+//         this.setState(prevState => {
+//             return {
+//                 profileData: [...prevState.profileData, profile]
+//             }
+//         })
+//     }
+//     render() {
+//         return (
+//             <div>
+//                 <div className="header">{this.props.title}</div>
+//                 <Form addCard={this.addProfileCardData}/>
+//                 <CardList profiles={this.state.profileData}/>
+//             </div>
+//         );
+//     }
+//
+// }
+function App(props) {
+    const [profileData, setProfileData] = useState([])
+    const addProfileCardData = (profile) => {
+        setProfileData(prevState => {
+            return [...prevState, profile]
         })
     }
-    render() {
-        return (
-            <div>
-                <div className="header">{this.props.title}</div>
-                <Form addCard={this.addProfileCardData}/>
-                <CardList profiles={this.state.profileData}/>
-            </div>
-        );
-    }
+    return(
+        <div>
+            <div className={"header"}>{props.title}</div>
+            <Form addCard={addProfileCardData}/>
+            <CardList profiles={profileData}/>
+        </div>
+
+    )
 
 }
 
