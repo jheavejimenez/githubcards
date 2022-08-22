e3853e7a907765e36d8b90f797ddb54c36cc0fa6
jheave <jimenezjheave123@gmail.com>
2022-08-23 01:35:51 +0800

    fix form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,10 +1,23 @@
 import React, {Component} from "react";
 
 class Form extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            userName: ""
+        }
+    }
+    handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(this.state.userName)
+    }
+
     render() {
         return (
-            <form action={""}>
+            <form onSubmit={this.handleSubmit}>
                 <input
+                    value={this.state.userName}
+                    onChange={(e) => this.setState({userName: e.target.value})}
                     type={"text"}
                     placeholder={"Github Username"}
                 />
