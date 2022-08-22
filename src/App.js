921102f64c6a8023e3742761bdfd4ff5e63b3a76
jheave <jimenezjheave123@gmail.com>
2022-08-23 01:17:22 +0800

    add form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, {Component} from "react";
 import CardList from "./components/CardList";
+import Form from "./components/Form";
 
 class App extends Component {
     render() {
         return (
             <div>
                 <div className="header">{this.props.title}</div>
+                <Form />
                 <CardList/>
             </div>
         );
