c7209fa89b7475f793bcec3b00ed23cf0b8ef715
jheave <jimenezjheave123@gmail.com>
2022-08-23 01:06:47 +0800

    refractor

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -9,7 +9,11 @@ class CardList extends Component {
                 avatar_url: "https://avatars0.githubusercontent.com/u/810438?v=4",
                 company: "@facebook"
             },
-            {name: "Sophie Alpert", avatar_url: "https://avatars2.githubusercontent.com/u/6820?v=4", company: "Humu"},
+            {
+                name: "Sophie Alpert",
+                avatar_url: "https://avatars2.githubusercontent.com/u/6820?v=4",
+                company: "Humu"
+            },
             {
                 name: "Sebastian Markbåge",
                 avatar_url: "https://avatars2.githubusercontent.com/u/63648?v=4",
