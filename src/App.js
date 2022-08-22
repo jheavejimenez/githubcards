aee4dc0345f466b5fc24218c1c976f0669158ea7
jheave <jimenezjheave123@gmail.com>
2022-08-23 01:51:05 +0800

    add a new function add card

--- src/App.js
+++ src/App.js
@@ -3,6 +3,19 @@ import CardList from "./components/CardList";
 import Form from "./components/Form";
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            profileData: []
+        }
+    }
+    addCard = (profile) => {
+        this.setState(prevState => {
+            return {
+                profileData: [...prevState.profileData, profile]
+            }
+        })
+    }
     render() {
         return (
             <div>
